7b25396a299483d5924daddda6586e257e640cb6
sukritcharredsoul <sukritchoudhary16@gmail.com>
2025-10-07 21:06:32 +0530

    Boiler Folder Structure

---
+++ src/main/java/com.example.hms/Hospice.java
@@ -0,0 +1,11 @@
+package com.example.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hospice{
+    public static void main(String[] args) {
+        SpringApplication.run(Hospice.class , args) ;
+    }
+}
